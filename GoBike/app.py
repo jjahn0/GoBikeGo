e592b8b4db349dcaf0c860ca4c30f3a61acb4b0f
jjahn0 <35445039+jjahn0@users.noreply.github.com>
2018-08-07 23:12:26 -0700

    init

---
+++ GoBike/app.py
@@ -0,0 +1,64 @@
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+app = Flask(__name__)
+
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+db = SQLAlchemy(app)
+
+
+# from .models import GoBike
+
+# @app.before_first_request
+# def setup():
+#     # Recreate database each time for demo
+#     # db.drop_all()
+#     db.create_all()
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/process")
+def process():
+    return render_template("process.html")
+
+@app.route("/data")
+def data():
+    return render_template("process.html")
+
+@app.route("/parallel")
+def parallel():
+    return render_template("parallel.html")
+
+@app.route("/chords")
+def chords():
+    iframe = 'chord_01.html'
+    return render_template("chords.html", iframe=iframe)
+
+@app.route("/tableau")
+def tableau():
+    return render_template("tableau.html")
+
+@app.route("/sklearn")
+def sklearn():
+    return render_template("tableau.html")
+
+@app.route("/NeuralNetwork")
+def NeuralNetwork():
+    return render_template("neuralnetwork.html")    
+
+
+# @app.route("/api/gobikes")
+# def pals():
+#     return jsonify(bike_data)
+
+if __name__ == "__main__":
+    app.run()
